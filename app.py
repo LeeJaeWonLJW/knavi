deb05a55e7d9ca4db297463ca56061c0f80cdcf0
Optatio <jaewon@jaewon.me>
2019-06-05 04:03:11 +0900

    fix

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from werkzeug import secure_filename
 
 import predict_kw
@@ -12,9 +12,18 @@ def hello():
 @app.route("/cnn", methods = ['POST'])
 def cnn():
     f = request.files['file']
-    data = open(secure_filename(f.filename), 'rb')
-    print(data)
-    return 'test'
+    data = f.read()
+    name, score = predict_kw.getScore(data)
+    sort_score = sorted(score)
+    result=None
+    for i in range(0, len(score), 1):
+        if score[i] == sort_score[len(sort_score)-1]:
+            result = (name[i], score[i])
+            break
+    response = {
+        "name": result[0]
+    }
+    return jsonify(response), 200
 
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=5000)
