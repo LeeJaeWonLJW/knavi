c0220db8b0c0ed913d8fa566ed6046fc1272d396
Optatio <jaewon@jaewon.me>
2019-06-05 03:33:14 +0900

    fix

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+import predict_kw
+
 app=Flask(__name__)
 
 @app.route("/")
 def hello():
     return "<h1>Hello Python Flask</h1>"
+
+@app.route("/cnn", methods = ['POST'])
+def cnn():
+    f = request.files['file']
+    data = open(f, 'rb')
+    print(data)
+    return 'test'
 
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=5000)
--- predict_kw.py
+++ predict_kw.py
@@ -1,12 +1,8 @@
 #-*- coding: utf-8 -*-
 import tensorflow as tf
-# import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import sys
-matplotlib.use('tkagg')
-
 
 tf.app.flags.DEFINE_string("output_graph",
                            "./workspace/kw_graph.pb",
@@ -14,14 +10,10 @@ tf.app.flags.DEFINE_string("output_graph",
 tf.app.flags.DEFINE_string("output_labels",
                            "./workspace/kw_labels.txt",
                            "학습할 레이블 데이터 파일")
-tf.app.flags.DEFINE_boolean("show_image",
-                            True,
-                            "이미지 추론 후 이미지를 보여줍니다.")
 
 FLAGS = tf.app.flags.FLAGS
 
-
-def main(_):
+def getScore(image):
     labels = [line.rstrip() for line in tf.gfile.GFile(FLAGS.output_labels)]
 
     with tf.gfile.FastGFile(FLAGS.output_graph, 'rb') as fp:
@@ -31,26 +23,12 @@ def main(_):
 
     with tf.Session() as sess:
         logits = sess.graph.get_tensor_by_name('final_result:0')
-        image = tf.gfile.FastGFile(sys.argv[1], 'rb').read()
         prediction = sess.run(logits, {'DecodeJpeg/contents:0': image})
 
-    # print('=== 예측 결과 ===')
-    # top_result = int(np.argmax(prediction[0]))
-    # name = labels[top_result]
-    # score = prediction[0][top_result]
-    # print('%s (%.2f%%)' % (name, score * 100))
-
-    print('=== 예측 결과 ===')
-    for i in range(len(labels)):
-        name = labels[i]
-        score = prediction[0][i]
-        print('%s (%.2f%%)' % (name, score * 100))
-
-    if FLAGS.show_image:
-        img = mpimg.imread(sys.argv[1])
-        plt.imshow(img)
-        plt.show()
-
+    score = list()
+    for tmp in prediction[0]:
+        score.append("%.2f" % (tmp * 100))
+    return labels, score
 
 if __name__ == "__main__":
     tf.app.run()
